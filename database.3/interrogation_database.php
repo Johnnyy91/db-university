5425ea99e9bf4876f99cc598cc40c4353d240e39
Johnnyy91 <claudiosardilli@gmail.com>
2022-10-24 17:04:29 +0200

    exercise complete

---
+++ database.3/interrogation_database.php
@@ -0,0 +1,28 @@
+<?php
+
+define("DB_SEVERNAME", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "db_university");
+define("DB_PORT", 8889);
+
+$connection = new mysqli(DB_SEVERNAME, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
+if( $connection && $connection->connect_error) {
+    echo "Error" . $connection->connect_error;
+    die();
+}
+
+$sql = "SELECT * FROM `students`";
+
+if($result = $connection->query($sql)){
+    
+    if($result->num_rows > 0){
+        
+    while($n_students = $result->fetch_assoc()){
+        echo "<h3>". $n_students['name'] . " " . $n_students['surname']. " " .$n_students['date_of_birth']. "</h3>"; 
+        
+    }
+    }
+}
+?>
